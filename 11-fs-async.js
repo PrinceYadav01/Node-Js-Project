afeb906f9c786877c0e9a47d099e6445f8910588
PrinceYadav1 <prince.y@osmosys.asia>
2022-04-11 13:29:00 +0530

    Promisify async fileSystem

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,29 +1,44 @@
-const {readFile, writeFile } = require('fs');
+const { readFile, writeFile } = require('fs').promises
 
-console.log('Start');
-readFile('./content/first.txt', 'utf8',(err,result)=>{
-    if (err){
-        console.log(err);
-        return;
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt','utf-8')
+        const second = await readFile('./content/second.txt','utf-8')
+        await writeFile('./content/result-promises.txt', `This is SUPER !!  ${first} ${second}`, {flag: 'a'})
+    } catch (error) {
+        console.log(error)
     }
-    const first = result;
-    readFile('./content/second.txt','utf8',(err,result)=>{
-        if(err) {
-            console.log(err)
-            return
-        }
-        const second = result;
-        writeFile('./content/result-async.txt',
-        `Here is the result : ${first}, ${second}`,
-        (err, result) => {
-            if(err){
-                console.log(err);
-                return;
-            }
-            console.log('Done with the task.');
-        }
-        );
-    });
-});
+}
+start()
 
-console.log('Starting the next one');
+
+// const getText = path => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// // getText('./content/first.txt').then((data) => console.log(data)).catch((err) => console.log(err))
+
+// const start = async()=> {
+//     try {
+//         const first = await getText('./content/first.txt')
+//         const second = await getText('./content/second.txt')
+//         console.log(first,second)
+//     } catch (error) {
+//         console.log(first)
+//     }
+    
+// }
+
+// start()
--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 const { readFile, writeFile } = require('fs').promises
+
 // const util = require('util')
 // const readFilePromise = util.promisify(readFile)
 // const writeFilePromise = util.promisify(writeFile)
 
 const start = async () => {
     try {
-        const first = await readFile('./content/first.txt', 'utf8');
-        const second = await readFile('./content/second.txt', 'utf8');
-        console.log(first, second)
-        await writeFile(
-            './content/result-mind-grenade.txt',
-            `This is Awesome : ${first} ${second}`,
-            { flag: 'a'}
-        )
+        const first = await readFile('./content/first.txt','utf-8')
+        const second = await readFile('./content/second.txt','utf-8')
+        await writeFile('./content/result-promises.txt', `This is SUPER !!  ${first} ${second}`, {flag: 'a'})
     } catch (error) {
         console.log(error)
     }
 }
-
 start()
 
-// const getText = (path) => {
+
+// const getText = path => {
 //     return new Promise((resolve, reject) => {
-//         readFile(path, 'utf8', (err, res) => {
+//         readFile(path, 'utf-8', (err, data) => {
 //             if (err) {
 //                 reject(err)
+//             } else {
+//                 resolve(data)
 //             }
-//             resolve(res)
 //         })
 //     })
-
 // }
 
-// const start = async () => {
+// // getText('./content/first.txt').then((data) => console.log(data)).catch((err) => console.log(err))
+
+// const start = async()=> {
 //     try {
-//         const first = await getText('./content/first.txt');
-//         const second = await getText('./content/second.txt');
-//         console.log(first, second)
+//         const first = await getText('./content/first.txt')
+//         const second = await getText('./content/second.txt')
+//         console.log(first,second)
 //     } catch (error) {
-//         console.log(error)
+//         console.log(first)
 //     }
+    
 // }
 
 // start()
